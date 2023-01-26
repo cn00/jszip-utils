ff4a30cba9dd725247a490584e36614c1b5743f1
Haijun <haijun.leng@smith-nephew.com>
2023-01-26 12:16:05 +0800

    use jstree

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -3,6 +3,8 @@ var params = URLSearchParams && new URLSearchParams(document.location.search.sub
 var zipuri = params && params.get("url");
 console.log("zipuri:", zipuri);
 
+var endn = zipuri.indexOf('?')
+var file_name = zipuri.substring(zipuri.lastIndexOf('/') + 1, endn > 0 ? endn : zipuri.length)
 // 1) get a promise of the content
 var promise = new JSZip.external.Promise(function (resolve, reject) {
     JSZipUtils.getBinaryContent(zipuri, function(err, data) {
@@ -10,9 +12,8 @@ var promise = new JSZip.external.Promise(function (resolve, reject) {
             console.error("getBinaryContent err:", err);
             reject(err);
         } else {
-            var endn = zipuri.indexOf('?')
             $("#file_name").replaceWith($("<div>", {id:"file_name"}).append($('<p>', {
-                text: zipuri.substring(zipuri.lastIndexOf('/') + 1, endn > 0 ? endn : zipuri.length)
+                text: file_name
             })));
 
             resolve(data);
@@ -37,40 +38,42 @@ function preview(fp){
             text: e.message
         })));
     })
-}    promise
+}
+promise
 .then(f=>{
     return JSZip.loadAsync(f)
 })                            // 2) chain with the zip promise
 .then(function success(zip) {
     zipf = zip;
 
+    var jsdata = []
     zip.forEach(function (relativePath, zipEntry) {  // 2) print entries
-        if(relativePath[relativePath.length-1]==='/')return;
-        $("#file_list").append($("<li>")
-        .append($("<a>", {
-            //href : "javascript:;",// + zipEntry.name,
-            href : "#" + zipEntry.name,
-            text: relativePath,//zipEntry.name,
-            // todo: use link action to add content preview
-            onclick: "preview('"+zipEntry.name+"')"
-        })));
+        if(relativePath[relativePath.length-1]==='/'){
+            relativePath = relativePath.substring(0, relativePath.lastIndexOf('/'))
+        }
+        jsdata[jsdata.length] = {
+            'id': relativePath,//.substring(relativePath.lastIndexOf('/')),
+            'parent': relativePath.substring(0, relativePath.lastIndexOf('/')) || '#', //file_name
+            'text': relativePath.substring(relativePath.lastIndexOf('/')),
+        }
     });
+    //  console.log("jsdata", jsdata)
+     $('#file_list')
+     // 监听事件
+    .on('changed.jstree', function (e, data) {
+        // console.log('Selected: ', data);
+        preview(data.node.id)
+        // 多选
+        // var i, j, r = [];
+        // for(i = 0, j = data.selected.length; i < j; i++) {
+        //     r.push(data.instance.get_node(data.selected[i]).id);
+        // }
+    })
+    .jstree({"core":{"data": jsdata}});
+
 }, function error(e) {
     $("#jszip_utils").append($("<div>", {
         "class" : "alert alert-danger",
         text : "Error reading " + e.message
     }));
 })
-//.then(function success(text) {                    // 4) display the result
-//    console.log("success(text)", text);
-//    $("#jszip_utils").append($("<p>", {
-//        "class": "alert alert-success",
-//        text: "loaded, content = " + text
-//    }));
-//}, function error(e) {
-//    console.error("error(e)", e);
-//    $("#jszip_utils").append($("<p>", {
-//        "class": "alert alert-danger",
-//        text: e
-//    }));
-//});
